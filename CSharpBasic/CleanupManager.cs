71f6391b492ab9ff57d0b846904f190d51910324
Barbieru Marian <barbierumarian13@gmail.com>
2022-05-17 23:48:44 +0300

    created a new Class for developing new functionality regarding closing the browser

---
+++ CSharpBasic/CleanupManager.cs
@@ -0,0 +1,12 @@
+﻿using OpenQA.Selenium.Chrome;
+
+namespace CSharpBasic
+{
+    public class CleanupManager
+    {
+        public void CloseChromeBrowser(ChromeDriver chromeDriver)
+        {
+            //TODO
+        }
+    }
+}
