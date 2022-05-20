8bb431aa2f7ad0abf1db257e9f018bdf8bc5cb6f
Barbieru Marian <barbierumarian13@gmail.com>
2022-05-20 13:39:03 +0300

    added 3 more exercises

--- CSharpBasic/Exercises.cs
+++ CSharpBasic/Exercises.cs
@@ -16,6 +16,19 @@ namespace CSharpBasic
 
         //4 - write a method that takes a message and just prints that message to the console. Example: Duhamel --> in the console the word Duhamel will appear
 
+        //5 - write a method called OpenCarWindow that takes the speed of the car as an INT Parameter and returns true or false whether it can open the window:
+            // speed < 10 --> it can open the window
+            // speed = 10 ---> it can open the window but in adition to that it show you the message "If you go faster I wont be able to open it"
+            // speed > 10 --> it cant open the window 
+
+        //6 -  write a program in C# Sharp to create a function to input a string and count number of spaces are in the string
+            //ex: This is a test string. --> will return value 4 (because there are 4 spaces)
+
+        //7 - write a method called TellMeIfItsSunnyOutsiode that will take an int parameter as the temperature and:
+            // if the temperature is greater than 30 it will show the message "Yes, it's hot outside" to the console
+            // if is between 15 and 30 it will show the message "It is sunny but take a coat with you"
+            // below 15 - "It's not sunny today outside"
+
         //Solution for 1)
 
         public int Multiply(int a, int b)
